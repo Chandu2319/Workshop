fedfa84d3db5187293e07aebca04010e93dee64e
Chandu2319 <bcsekhar2319@gmail.com>
2020-09-26 13:29:17 +0530

    Use case 6 choosing first player through Heads/Tails.

--- src/classes/Tic_tac_toe.java
+++ src/classes/Tic_tac_toe.java
@@ -22,7 +22,35 @@ public class Tic_tac_toe {
 		}
 	}
 	
-	void chooseSymbol(){ 		//UC2
+	void choosePlayerByToss(){
+		int input;
+		System.out.println("Choose Heads or Tails:\n1.Heads\n2.Tails" );
+		Scanner scan = new Scanner(System.in);
+		while(true){
+			input = scan.nextInt();
+			if(input == 1 || input == 2 ){
+				break;
+			}else {
+				System.out.println("Enter either 1 or 2 only");
+			}
+		}
+		int toss = playToss();
+		if(input == toss){
+			System.out.println("Player wins and has the first move");
+		} else {
+			System.out.println("Computer wins and has the first move");
+		}
+	}
+	
+	int playToss(){
+		int output;
+		if(Math.random()%2 == 0){
+			output = 1;
+		}else {output = 2;}
+		return output;
+	}
+	
+	void chooseSymbol() throws IOException{ 		//UC2
 		String input = new String();
 		System.out.println("Choose your symbol(X or O) :");
 		Scanner scan = new Scanner(System.in);
@@ -48,16 +76,16 @@ public class Tic_tac_toe {
 		System.out.println(board[7]+"|"+board[8]+"|"+board[9]);
 	}
 	
-	void insertSymbol() { 		//UC4 and UC5
+	void insertSymbol() throws IOException{ 		//UC4 and UC5
 		int input = 0;
-		System.out.println("Enter the position(1-9) at which you place your symbol : ");
+		System.out.println("Enter the position(1-9) at which you want to place your symbol : ");
 		Scanner scan = new Scanner(System.in);
         while(true){
         	input = scan.nextInt();
         	if(isvalid(input)){
         		break;
         	} else {
-        		System.out.println("Enter a position which is available");
+        		System.out.println("Enter a valid position which is available\n(numbering is done horizontal first)");
         	}
         }
         scan.reset();
@@ -75,12 +103,14 @@ public class Tic_tac_toe {
 		return output;
 	}
 	
-	public static void main(String args[]){
+	public static void main(String args[]) throws IOException{
 		Tic_tac_toe game1 = new Tic_tac_toe();
 		game1.initializeBoard();		//UC1
 		game1.chooseSymbol(); 			//UC2
+		game1.choosePlayerByToss(); 	//UC6
 		game1.displayBoard();			//UC3
-		game1.insertSymbol(); 			//UC4
-		game1.displayBoard(); 			//UC5
+		game1.insertSymbol(); 			//UC4&5
+		game1.displayBoard(); 			//UC4&5
+		
 	}
 }
