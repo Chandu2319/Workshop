85477d97bbc3463a362fbb25a09868bdb6fc373c
Chandu2319 <bcsekhar2319@gmail.com>
2020-09-26 10:55:21 +0530

    Use case 2 Choosing symbol

--- src/classes/Tic_tac_toe.java
+++ src/classes/Tic_tac_toe.java
@@ -22,11 +22,31 @@ public class Tic_tac_toe {
 		}
 	}
 	
-	
+	void choose_symbol(){
+		String input;
+		System.out.println("Choose your symbol(X or O) :");
+		Scanner scan = new Scanner(System.in);
+        input = scan.next();
+        while(input.isEmpty()){
+        	if(input == "X"){
+        		this.player = input.charAt(0);
+        		this.computer = 'O';
+        	}
+        	else if(input == "O"){
+        		this.player = input.charAt(0);
+        		this.computer = 'X';
+        	}
+        	else{
+        		System.out.println("Enter characters either X or O only");
+        		input="";
+        	}
+        	scan.close();
+        }
+	}
 	
 	public static void main(String args[]){
 		Tic_tac_toe game1 = new Tic_tac_toe();
 		game1.Initialize_board();		//UC1
-		
+		game1.choose_symbol(); 			//UC2
 	}
 }
