f423a6c36d9e8003ff0622c561f9481b9002a226
Chandu2319 <bcsekhar2319@gmail.com>
2020-09-26 11:16:01 +0530

    Use case 3 Displaying the board

--- src/classes/Tic_tac_toe.java
+++ src/classes/Tic_tac_toe.java
@@ -11,13 +11,13 @@ public class Tic_tac_toe {
 	char player;
 	char computer;
 	
-	public void Tic_tac_toe() {
-		char[] board = new char[10];
+	public Tic_tac_toe() {
+		board = new char[10];
 	}
 	
-	void Initialize_board() {
+	void initialize_board() {
 		// UC1
-		for(int i=1; i<=10; i++){
+		for(int i=1; i<10; i++){
 			this.board[i] = '0';
 		}
 	}
@@ -44,9 +44,15 @@ public class Tic_tac_toe {
         }
 	}
 	
+	void display_board() {
+		System.out.println(board[1]+"|"+board[2]+"|"+board[3]);
+		
+	}
+	
 	public static void main(String args[]){
 		Tic_tac_toe game1 = new Tic_tac_toe();
-		game1.Initialize_board();		//UC1
+		game1.initialize_board();		//UC1
 		game1.choose_symbol(); 			//UC2
+		game1.display_board();
 	}
 }
